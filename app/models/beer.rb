488dc51be5cabe9aa4e735e7fb9d3d776a549ea9
Valtis <erkka.kaaria@helsinki.fi>
2015-01-24 13:13:43 +0200

    3 & 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,8 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    ratings.inject(0) { |total, rating| total + rating.score } / ratings.count
+  end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,8 @@
 class Rating < ActiveRecord::Base
   belongs_to :beer
+
+
+  def to_s
+    "Beer: #{beer.name} Rating: #{score}"
+  end
 end
