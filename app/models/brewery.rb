7ff77360a5e2dc65f3e5f2760a5a36a43277d271
Valtis <erkka.kaaria@helsinki.fi>
2015-01-31 14:27:52 +0200

    6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,9 +4,14 @@ class Brewery < ActiveRecord::Base
   has_many :ratings, through: :beers
   validates :name, presence: true
 
-  validates :year, numericality: { greater_than_or_equal_to: 1042,
-                                   less_than_or_equal_to: 2015,
-                                   only_integer: true }
+  validates :year, numericality: { greater_than_or_equal_to: 1042, only_integer: true }
+  validate :year_cannot_be_in_the_future
+
+  def year_cannot_be_in_the_future
+    if Date.today.year < year
+      errors.add(:year, " cannot be in the future (for time travellers, current year is #{Date.today.year}")
+    end
+  end
 
 
 
