3432844b184a123f68a58899cbe3de81099c688d
Valtis <erkka.kaaria@helsinki.fi>
2015-01-31 14:39:17 +0200

    many-many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   include AverageRating
   has_many :ratings
+  has_many :beers, through: :ratings
 
   validates :username, uniqueness: true,  length: { minimum: 3, maximum: 15 }
 end
