cdd3ceecfcca238130f40b6d8b07d7a2e45dd33c
Valtis <erkka.kaaria@helsinki.fi>
2015-01-31 13:40:37 +0200

    2

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
 
   get 'signup', to:  'users#new'
 
-  get 'login', to: 'sessions#new'
+  get 'signin', to: 'sessions#new'
+
+  delete 'signout', to: 'sessions#destroy'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
