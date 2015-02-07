1081f92e8c548d7f8f287aad5eb0997b96bd7ef7
Valtis <erkka.kaaria@helsinki.fi>
2015-02-07 18:43:09 +0200

    14

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,4 +1,6 @@
 class BeersController < ApplicationController
+
+  before_action :ensure_that_signed_in, except: [:index, :show]
   before_action :set_beer, only: [:show, :edit, :update, :destroy]
   before_action :set_new_edit_data, only: [:new, :edit, :create]
   # GET /beers
