a691f73fd59973fa8e9b441e74d6658faeb5d14e
Valtis <erkka.kaaria@helsinki.fi>
2015-02-07 17:38:54 +0200

    8 & 9

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -5,13 +5,21 @@ describe "User" do
   before :each do
     @user = FactoryGirl.create :user
     @user2 = FactoryGirl.create(:user, username: "Marko")
+
+    @user3 = FactoryGirl.create(:user, username: "Petteri")
     brewery = FactoryGirl.create :brewery
     @beer = FactoryGirl.create :beer, name:"iso 3", brewery:brewery
     @beer2 = FactoryGirl.create :beer, name:"testi", brewery:brewery
 
+    @r1 = Rating.create beer: @beer, user: @user, score: 20
+    @r2 = Rating.create beer: @beer2, user: @user, score: 30
+    @r3 = Rating.create beer: @beer, user: @user2, score: 10
+
   end
 
   describe "who has signed up" do
+
+
     it "can signin with right credentials" do
       sign_in(username:"Pekka", password:"Foobar1")
 
@@ -37,21 +45,34 @@ describe "User" do
       }.to change{User.count}.by(1)
     end
 
-    it "has ratings listed in their userpage" do
-
-      r1 = Rating.create beer: @beer, user: @user, score: 20
-      r2 = Rating.create beer: @beer2, user: @user, score: 30
-      r3 = Rating.create beer: @beer, user: @user2, score: 10
-
+    it "and has reviews lists users favorite brewery and style" do
       visit user_path(@user)
-
-
-      expect(page).to have_content "#{@user.username} has made 2 ratings"
-      expect(page).to have_content "#{r1}"
-      expect(page).to have_content "#{r2}"
-      expect(page).not_to have_content "#{r3}"
-
+      expect(page).to have_content 'Favorite brewery: anonymous'
+      expect(page).to have_content 'Favorite style: Lager'
     end
 
+    it "and has no reviews lists notification about no style and brewery" do
+      visit user_path(@user3)
+      expect(page).to have_content 'Favorite brewery: No favorite brewery yet '
+      expect(page).to have_content 'Favorite style: No favorite style yet '
+    end
+
+  end
+
+  describe "who has signed in" do
+    before :each do
+      sign_in(username:"Pekka", password:"Foobar1")
+    end
+
+    it "can delete a rating they have made" do
+      visit user_path(@user)
+      puts(@r2)
+
+      page.find('li', :text => @r2.to_s).click_link 'delete'
+
+      expect(@user.ratings.count).to eq(1)
+      expect(@user.ratings.first).to eq(@r1)
+
+    end
   end
 end
